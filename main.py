6eecf2ec5d15a8b69f3421c584f1485ed0ed07d5
Farhan Azmi <68798786+farhanazmiCS@users.noreply.github.com>
2024-11-12 12:09:21 +0800

    Wrote API endpoint code for model

---
+++ main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, File, HTTPException, UploadFile
+import librosa
+from model.model import WhisperForSingaporeAphasia
+from tempfile import NamedTemporaryFile
+
+VALID_AUDIO_TYPES = {
+    "audio/wav",
+    "audio/x-wav",
+    "audio/mpeg",
+    "audio/mp4",
+    "audio/x-m4a",
+    "audio/aac",
+    "audio/flac",
+    "audio/ogg",
+    "audio/x-aiff",
+    "audio/wma"
+}
+
+app = FastAPI()
+model = WhisperForSingaporeAphasia()
+
+@app.post("/transcribe/", status_code=200)
+async def trandscribe(audiofile: UploadFile = File(...)):
+    if audiofile.content_type not in VALID_AUDIO_TYPES:
+        raise HTTPException(
+            status_code=415,
+            detail="Unsupported audio file type. Please upload a valid audio file."
+        )
+    
+    # Writes the content to a temporary file, then load the waveform from there
+    try:
+        contents = audiofile.file.read()
+        temp = NamedTemporaryFile(delete=False)
+        with temp as f:
+            f.write(contents)
+        waveform, _ = librosa.load(temp.name, sr=16000)
+    except Exception as e:
+        raise HTTPException(
+            status_code=415,
+            detail=f"Librosa failed to extract audio waveform: {str(e)}"
+        )
+
+    try:
+        transcription = model.transcribe(waveform)
+    except Exception as e:
+        raise HTTPException(
+            status_code=415,
+            detail=f"Failed to transcribe audio: {str(e)}"
+        )
+
+    return {
+        "status": "SUCCESS",
+        "transcription": transcription
+    }
--- model/model.py
+++ model/model.py
@@ -1,12 +1,13 @@
-from transformers import AutoModelForSpeechSeq2Seq, WhisperProcessor
 import numpy as np
 import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
 
 class WhisperForSingaporeAphasia:
     def __init__(self):
         self.device = self._get_device()
         self.model = AutoModelForSpeechSeq2Seq.from_pretrained("f-azm17/whisper-small-singapore-aphasia").to(self.device)
-        self.processor = WhisperProcessor.from_pretrained("openai/whisper-small.en")
+        self.processor = AutoProcessor.from_pretrained("f-azm17/whisper-small-singapore-aphasia")
+        self.model.eval()
     
     def _get_device(self) -> str:
         if torch.backends.mps.is_available():
@@ -15,11 +16,7 @@ class WhisperForSingaporeAphasia:
             return "cuda"
         return "cpu"
     
-    def transcribe(self, waveform_path: str = '', sample_rate: int = 16000) -> str:
-        try:
-            waveform = np.load(waveform_path)
-        except:
-            raise OSError
+    def transcribe(self, waveform: np.ndarray, sample_rate: int = 16000) -> str:
         inputs = self.processor(waveform, sampling_rate=sample_rate, return_tensors="pt").to(self.device)
         with torch.no_grad():
             generated_ids = self.model.generate(inputs["input_features"])
