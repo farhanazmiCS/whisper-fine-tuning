f809f56113cde9ac478f3b0032fe077bf2e594fd
Farhan Azmi <68798786+farhanazmiCS@users.noreply.github.com>
2024-11-18 13:19:28 +0800

    Update temperature to 0.0

--- model/model.py
+++ model/model.py
@@ -16,14 +16,14 @@ class WhisperForSingaporeAphasia:
             return "cuda"
         return "cpu"
     
-    def transcribe(self, waveform: np.ndarray, sample_rate: int = 16000, input_prompt: str = '') -> str:
+    def transcribe(self, waveform: np.ndarray, sample_rate: int = 16000, input_prompt: str = "") -> str:
         inputs = self.processor(waveform, sampling_rate=sample_rate, return_tensors="pt").to(self.device)
         input_prompt_ids = self.processor.tokenizer(input_prompt, return_tensors="pt").input_ids.squeeze().to(self.device)
         with torch.no_grad():
             if input_prompt == "":
                 generated_ids = self.model.generate(
                     input_features=inputs["input_features"], 
-                    temperature=0.8
+                    temperature=0.0
                 )
             else:
                 generated_ids = self.model.generate(
